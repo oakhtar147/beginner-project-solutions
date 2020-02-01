3fd5396f9d36b6431be771c659fdd22db3af8887
OsamaAkhtar <57761675+OsamaAkhtar@users.noreply.github.com>
2020-02-01 14:23:35 +0500

    removed redundancy in conditional statement

--- 99-bottles.py
+++ 99-bottles.py
@@ -5,6 +5,7 @@ p = inflect.engine()
 numbers = range(99, 0, -1)
 worded_nums = range(1, 100)
 
+# worded_num contains English words for the given number range.
 worded_num = [p.number_to_words(num) for num in worded_nums]
 
 index = 0
@@ -13,16 +14,13 @@ lyric = '{} bottles of beer on the wall. {} bottles of beer.'
 lyric2 = '\nTook {} down and passed it around, {} bottles of beer on the wall.\n'
 
 for num in numbers:
-    if index == 0:
+    if index == 0 or num != 1:
         print((lyric+lyric2).format(num, num, worded_num[index], num-1))
         index += 1
     else:
         if num == 1:
             print((lyric.replace("bottles", "bottle") +
                    lyric2).format(num, num, worded_num[index], num-1))
-            index += 1
-        else:
-            print((lyric+lyric2).format(num, num, worded_num[index], num-1))
             index += 1
 
 print("No more bottles of beer on the wall, no more bottles of beer.\n" +
