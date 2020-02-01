decc67cb901aa57ba9b8b0e77ba724a1a2cb9da5
OsamaAkhtar <57761675+OsamaAkhtar@users.noreply.github.com>
2020-02-01 12:14:37 +0500

    intial commit

---
+++ 99-bottles.py
@@ -0,0 +1,29 @@
+import inflect
+
+p = inflect.engine()
+
+numbers = range(99, 0, -1)
+worded_nums = range(1, 100)
+
+worded_num = [p.number_to_words(num) for num in worded_nums]
+
+index = 0
+
+lyric = '{} bottles of beer on the wall. {} bottles of beer.'
+lyric2 = '\nTook {} down and passed it around, {} bottles of beer on the wall.\n'
+
+for num in numbers:
+    if index == 0:
+        print((lyric+lyric2).format(num, num, worded_num[index], num-1))
+        index += 1
+    else:
+        if num == 1:
+            print((lyric.replace("bottles", "bottle") +
+                   lyric2).format(num, num, worded_num[index], num-1))
+            index += 1
+        else:
+            print((lyric+lyric2).format(num, num, worded_num[index], num-1))
+            index += 1
+
+print("No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall.")
